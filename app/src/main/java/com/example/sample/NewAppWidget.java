85725420991d203dbc64f214e3c786fe82097bca
Hasan El-Hefnawy <helhefnawy89@yahoo.com>
2019-06-22 08:10:59 +0200

    Add ImageView and remove TextView in Widget layout

--- app/src/main/java/com/example/sample/NewAppWidget.java
+++ app/src/main/java/com/example/sample/NewAppWidget.java
@@ -13,10 +13,8 @@ public class NewAppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
